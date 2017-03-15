1d52814ec3267fac507376275a3de5fabd14b3cb
mavzer122 <mavzerburak0@gmail.com>
2017-03-15 11:05:37 +0200

    Update Divisors.py

--- Divisors.py
+++ Divisors.py
@@ -1,6 +1,6 @@
 user_number = raw_input("Please enter a number between 1 and 9999.\n > ")
 list_of_divisors = []
-for element in range(1,999):
+for element in range(1,9999):
 	if int(user_number) % element == 0:
 		list_of_divisors.append(element)
 		print list_of_divisors
