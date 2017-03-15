39b0cdc2ede9161c2f829d9ee18cf7a537edb50d
mavzer122 <mavzerburak0@gmail.com>
2017-03-15 16:41:36 +0200

    Update

--- ListLessThanUI.py
+++ ListLessThanUI.py
@@ -1,10 +1,11 @@
 from random import randint
 a = []
-b = []
 for random_integer in range(20):
-	a.append(randint(0, 999)
+	a.append(randint(0, 999))
 user_input = raw_input('Please enter a number. > ')
-for item in a:
-	if item <= int(user_input):
-		b.append(item)
-		print b
+b = [item for item in a if item <= int(user_input)]
+print sorted(b)
+#for item in a:
+	#if item <= int(user_input):
+		#b.append(item)
+		#print b
