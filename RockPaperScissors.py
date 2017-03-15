b42ad4dc9ba237f3c50ad5be26e10ea24dc35ca3
mavzer122 <mavzerburak0@gmail.com>
2017-03-15 22:26:39 +0200

    A Very Basic Rock Paper Scissors game in python
    To practice while loops

---
+++ RockPaperScissors.py
@@ -0,0 +1,42 @@
+import random
+print '''Welcome to my Rock, Paper, Scissors game.
+You will play 3 times against the computer.'''
+
+def rock_paper_scissors():
+	a = ["Rock", "Paper", "Scissors"]
+	plays = 0
+	while plays <= 2:
+		plays += 1
+		user_input = raw_input("Type Rock, Paper or Scissors: ")
+		computer_choice = random.choice(a)
+		if user_input == computer_choice:
+			print "Tie Game!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Rock" and computer_choice == "Paper":
+			print "You lose!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Rock" and computer_choice == "Scissors":
+			print "Congrats! You win!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Paper" and computer_choice == "Rock":
+			print "Congrats! You win!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Paper" and computer_choice == "Scissors":
+			print "You lose!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Scissors" and computer_choice == "Rock":
+			print "You lose!"
+			print "The computer chose " + computer_choice
+		elif user_input == "Scissors" and computer_choice == "Paper":
+			print "Congrats! You win!"
+			print "The computer chose " + computer_choice
+		else:
+			print "Please enter 'Rock', 'Paper' or 'Scissors'"
+
+rock_paper_scissors()
+while True:
+	play_again = raw_input("Type Yes to play again, type Quit to exit. ")
+	if play_again == "Yes":
+		rock_paper_scissors()
+	elif play_again == "Quit":
+		break
