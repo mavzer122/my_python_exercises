bf973f6b19070aea9485df8399e98df3437eb613
mavzer122 <mavzerburak0@gmail.com>
2017-03-16 14:13:03 +0200

    A more updated version of A Basic Guess Game

--- GuessGame1.py
+++ GuessGame1.py
@@ -6,10 +6,18 @@ def guess_game():
 		user_guess = raw_input("Please enter a number between 1 and 9. Type exit to quit. ")
 		if user_guess == "exit":
 			break
+		try:
+			user_guess = int(user_guess)
+		except:
+			print "That's not a number."
+			continue
+		if int(user_guess) > 9 or int(user_guess) < 1:
+			print "You should enter a number between 1 and 9."
 		elif int(user_guess) == random_number:
 			print "You guessed correctly!"
-			print "You have guessed " + str(guesses) + " times."
-			break
+			print "You have found the number in " + str(guesses) + " guesses."
+			random_number = randint(1, 9)
+			guesses -= guesses
 		elif int(user_guess) < random_number:
 			print "Too low!"
 			guesses += 1
